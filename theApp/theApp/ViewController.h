c12e9d38aee48dca08e09235dc55e11d2733d9c6
xiaoaoyintian <zhangjump@yeah.net>
2018-03-07 15:20:48 +0800

    Add files via upload

---
+++ theApp/theApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  theApp
+//
+//  Created by home on 2018/3/7.
+//  Copyright © 2018年 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ theApp/theApp/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  theApp
+//
+//  Created by home on 2018/3/7.
+//  Copyright © 2018年 home. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //self.view.backgroundColor = [UIColor blueColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
